3ab4b0137369dcbe19dae3362f909f17780dd172
icalajah <ahmad.aziz@wearesocialights.com>
2023-02-12 14:36:13 +0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Ahmad Aziz on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
